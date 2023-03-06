fbeec709b5443c305cf61aba65217ce8080385bc
AlyssaRamos1 <122499249+AlyssaRamos1@users.noreply.github.com>
2023-03-05 19:05:14 -0500

    appointment

--- shelterapp/src/app/app.module.ts
+++ shelterapp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { appointment } from './appointment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    appointment
   ],
   imports: [
     BrowserModule,
---
+++ shelterapp/src/app/appointment.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-appointment',
+    templateUrl: 'appointment.html',
+    styleUrls: ['appointment.css']
+})
+export class appointment {
+
+}
